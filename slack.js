48b23b21894f3ecfe74dfa62585e80360408a16b
Felix Bouleau <bouleau@spotify.com>
2014-04-15 12:56:40 +0000

    Adds icon_emoji and icon_url support

--- slack.js
+++ slack.js
@@ -2,27 +2,29 @@ var request=require('request');
 
 function Slack(domain,token) {
 	this.domain = domain;
-	this.token = token;	
+	this.token = token;
 }
 
 Slack.prototype.send = function(message,cb) {
-	
-	
+
+
 	if (!message.text) {
 		if (cb) cb.call(null,{message:'No text specified'},null);
 		return;
 	}
 	if (!message.channel) { message.channel = '#general'; }
-		
+
 	var command = 'https://' + this.domain + '.slack.com/services/hooks/incoming-webhook?token=' + this.token;
 	var options = {
-		
+
 		"channel":message.channel,
 		"text": message.text,
-		"username":message.username
-			
+		"username":message.username,
+		"icon_emoji":message.icon_emoji,
+		"icon_url":message.icon_url
+
 	};
-	
+
 	request.post({url:command,body:JSON.stringify(options)},function(e,r,body) {
 
 		// done!
@@ -31,16 +33,16 @@ Slack.prototype.send = function(message,cb) {
 			body = null;
 		}
 		if (cb) cb.call(null,e,body);
-				
+
 	});
-	
+
 }
 
 
 Slack.prototype.respond = function(query,cb) {
-	
+
 	var obj = {};
-	
+
 	obj.token = query.token;
 	obj.team_id = query.team_id;
 	obj.channel_id = query.channel_id;
@@ -49,13 +51,13 @@ Slack.prototype.respond = function(query,cb) {
 	obj.user_id = query.user_id;
 	obj.user_name = query.user_name;
 	obj.text = query.text;
-	
+
 	if (!cb) {
 		return {text:''};
 	} else {
 		return cb.call(null,obj);
 	}
-	
+
 }
 
 module.exports = Slack;
