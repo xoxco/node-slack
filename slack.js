96cf2d91369331bf6c082783cbeab5deb6b7431b
Ben Brown <ben@xoxco.com>
2014-07-03 20:34:15 -0500

    rename options

--- slack.js
+++ slack.js
@@ -3,10 +3,10 @@
 var request  = require('request');
 var deferred = require('deferred');
 
-function Slack(domain, token, option) {
+function Slack(domain, token, http_proxy_options) {
   this.domain = domain;
   this.token = token;
-  this.option = option;
+  this.http_proxy_options = http_proxy_options;
 }
 
 Slack.prototype.send = function(message, cb) {
@@ -24,7 +24,7 @@ Slack.prototype.send = function(message, cb) {
   };
 
   var option = {
-    proxy: (this.option && this.option.proxy) || process.env.https_proxy || process.env.http_proxy,
+    proxy: (this.http_proxy_options && this.http_proxy_options.proxy) || process.env.https_proxy || process.env.http_proxy,
     url:   command,
     body:  JSON.stringify(body)
   };
