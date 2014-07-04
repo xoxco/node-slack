1a993733c15f54a08477716c3545b47f08d8aef8
Ben Brown <ben@xoxco.com>
2014-07-03 20:22:24 -0500

    add option for unfurling links

--- slack.js
+++ slack.js
@@ -32,6 +32,7 @@ Slack.prototype.send = function(message, cb) {
 	if (message.icon_url) { options.icon_url = message.icon_url; }
 	if (message.icon_emoji) { options.icon_emoji = message.icon_emoji; }
 	if (message.attachments) { options.attachments = message.attachments; }
+  if (message.unfurl_links) { options.unfurl_links = message.unfurl_links; }
 
   if(!cb) var d = deferred();
 
