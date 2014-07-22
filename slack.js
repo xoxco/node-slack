8cd48b174d52b221ba097ab9842eecef22727aa1
Van Miranda <van@vurb.com>
2014-07-21 17:09:44 -0700

    Send slack options like link_names with request body

--- slack.js
+++ slack.js
@@ -23,16 +23,17 @@ Slack.prototype.send = function(message, cb) {
     username: message.username
   };
 
+  if (message.icon_url) { body.icon_url = message.icon_url; }
+  if (message.icon_emoji) { body.icon_emoji = message.icon_emoji; }
+  if (message.attachments) { body.attachments = message.attachments; }
+  if (message.unfurl_links) { body.unfurl_links = message.unfurl_links; }
+  if (message.link_names) { body.link_names = message.link_names; }
+
   var option = {
     proxy: (this.http_proxy_options && this.http_proxy_options.proxy) || process.env.https_proxy || process.env.http_proxy,
     url:   command,
     body:  JSON.stringify(body)
   };
-
-	if (message.icon_url) { options.icon_url = message.icon_url; }
-	if (message.icon_emoji) { options.icon_emoji = message.icon_emoji; }
-	if (message.attachments) { options.attachments = message.attachments; }
-  if (message.unfurl_links) { options.unfurl_links = message.unfurl_links; }
 
   if(!cb) var d = deferred();
 
